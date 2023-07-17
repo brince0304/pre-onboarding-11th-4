2d03b294129997b8ad496b09e6c3c5d28b532c13
brince0304 <brince0304@gmail.com>
2023-07-17 23:42:16 +0900

    fix: RecommendBox 컴포넌트 props 수정

--- src/components/RecommendBox/RecommendBox.tsx
+++ src/components/RecommendBox/RecommendBox.tsx
@@ -8,7 +8,7 @@ import useSickList from '../../hooks/useSickList';
 import Loading from '../ResultList/Loading';
 import NoResult from '../ResultList/NoResult';
 
-const RecommendBox = ({ input, setInput, sickList, showRecentQueries, selectedIndex }: IRecommendBoxProps) => {
+const RecommendBox = ({ input, submitHandler, sickList, showRecentQueries, selectedIndex }: IRecommendBoxProps) => {
   const { loading } = useSickList();
   const isLoading = loading === 'pending';
   const showInputKeyword = !isLoading && input.length > 0;
@@ -18,17 +18,17 @@ const RecommendBox = ({ input, setInput, sickList, showRecentQueries, selectedIn
     <S.Container>
       {isLoading && <Loading />}
       {showInputKeyword && <ResultItem sick={{ sickCd: '', sickNm: input }} />}
-      {showRecentQueries && <RecentQueries setInput={setInput} />}
+      {showRecentQueries && <RecentQueries onClickHandler={submitHandler} />}
       {showResultList && <ResultList sickList={sickList} selectedIndex={selectedIndex} />}
       {showNoResult && <NoResult />}
-      {showRecentQueries && <RecommendQueryButtons setInput={setInput} />}
+      {showRecentQueries && <RecommendQueryButtons onClickHandler={submitHandler} />}
     </S.Container>
   );
 };
 
 interface IRecommendBoxProps {
   input: string;
-  setInput: (input: string) => void;
+  submitHandler: (...args: any[]) => void;
   sickList?: ISickList;
   showRecentQueries: boolean;
   selectedIndex: number;
--- src/components/ResultList/NoResult.tsx
+++ src/components/ResultList/NoResult.tsx
@@ -2,9 +2,9 @@ import { Box, Typography } from '@mui/material';
 
 const NoResult = () => {
   return (
-    <Box sx={{ marginLeft: '20px' }}>
+    <Box sx={{ marginLeft: '20px', padding: '10px 0px' }}>
       <Typography fontSize={'14px'} color={'#999'}>
-        검색어가 존재하지 않습니다.
+        추천 검색어가 존재하지 않습니다.
       </Typography>
     </Box>
   );
