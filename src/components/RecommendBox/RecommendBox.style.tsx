1181245a5b6d502abb3355acce849a1f7ede650c
brince0304 <brince0304@gmail.com>
2023-07-17 12:40:24 +0900

    refactor: 추천검색어창 가독성 리팩토링

--- src/components/RecommendBox/RecommendBox.style.tsx
+++ src/components/RecommendBox/RecommendBox.style.tsx
@@ -1,15 +1,16 @@
 import styled from '@emotion/styled';
+import { Box } from '@mui/material';
 
-export const Container = styled.div`
+export const Container = styled(Box)`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
   background-color: #ffffff;
   border-radius: 30px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
   position: absolute;
   top: 120%;
+  padding: 15px 0;
   left: 0;
-  z-index: 1;
+  z-index: 10;
 `;
--- src/components/RecommendBox/RecommendBox.tsx
+++ src/components/RecommendBox/RecommendBox.tsx
@@ -1,22 +1,33 @@
 import * as S from './RecommendBox.style';
 import { ISickList } from '../../interfaces/iSickList';
-const RecommendBox = ({ sickList }: IRecommendBoxProps) => {
+import ResultList from '../SearchBox/ResultList/ResultList';
+import RecentQueries from './RecentQuery/RecentQueries';
+import ResultItem from '../SearchBox/ResultList/ResultItem';
+import RecommendQueryButtons from './RecommendQueries/RecommendQueryButtons';
+import useSickList from '../../hooks/useSickList';
+import Loading from '../SearchBox/Loading';
+
+const RecommendBox = ({ input, setInput, sickList, showRecentQueries }: IRecommendBoxProps) => {
+  const showInputKeyword = input.length > 0;
+  const showResultList = sickList && sickList.length > 0;
+  const { loading } = useSickList();
+  const isLoading = loading === 'pending';
   return (
     <S.Container>
-      {sickList &&
-        sickList.map((sick, index) => {
-          return (
-            <div>
-              <div>{sick.sickNm}</div>
-            </div>
-          );
-        })}
+      {isLoading && <Loading />}
+      {!isLoading && showInputKeyword && <ResultItem sick={{ sickCd: '', sickNm: input }} onClick={() => {}} />}
+      {showRecentQueries && <RecentQueries />}
+      {!isLoading && showResultList && <ResultList sickList={sickList} />}
+      {showRecentQueries && <RecommendQueryButtons setInput={setInput} />}
     </S.Container>
   );
 };
 
 interface IRecommendBoxProps {
+  input: string;
+  setInput: (input: string) => void;
   sickList?: ISickList;
+  showRecentQueries: boolean;
 }
 
 export default RecommendBox;
