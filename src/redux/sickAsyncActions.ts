b719c58b1e12ccb574b16b214afa4aa7e91b04fd
brince0304 <brince0304@gmail.com>
2023-07-17 12:37:22 +0900

    feat: 추천 검색어 가져오기 액션 추가

--- src/redux/sickAsyncActions.ts
+++ src/redux/sickAsyncActions.ts
@@ -1,12 +1,23 @@
 import { RootState } from './store';
 import { iSickChild } from '../interfaces/iSickList';
 
-export const getSickListByQueryFromCachedList = (query: string) => {
+export const getSickListByQueryFromCachedListAction = (query: string) => {
   return async (dispatch: any, getState: any) => {
     const state = getState() as RootState;
-    const cachedList = state.sickReducer.sickCache.find((sickCache) => sickCache.query === query);
-    if (cachedList) {
-      return cachedList.sickList as iSickChild[];
+    const cachedSickList = state.sickReducer.sickCache.find((sickCache) => sickCache.query === query);
+    if (cachedSickList) {
+      return cachedSickList.sickList as iSickChild[];
     }
+  };
+};
+export const getRecommendQueryAction = () => {
+  return async (dispatch: any, getState: any) => {
+    const state = getState() as RootState;
+    const sickCache = state.sickReducer.sickCache;
+    const sickList = sickCache.filter(
+      (sick) => sick.query.length >= 2 && sick.sickList.length < 3 && sick.sickList.length > 0,
+    );
+    const recommendQuery = sickList.map((sick) => sick.query);
+    return recommendQuery.splice(0, 5) as string[];
   };
 };
