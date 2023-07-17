06395f3c0fc517fc0fe7708dac8c4012bdba1632
brince0304 <brince0304@gmail.com>
2023-07-17 12:39:24 +0900

    feat: 최근 검색기록 가져오기 기능 구현

--- src/context/sickContext.tsx
+++ src/context/sickContext.tsx
@@ -1,7 +1,9 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { SickServiceInterface } from '../service/sickService';
+import { ISickList } from '../interfaces/iSickList';
+import { sessionStorageQueryListName } from '../utils/sickUtility';
 
-const sickServiceContext = createContext<SickServiceInterface>({} as SickServiceInterface);
+const sickServiceContext = createContext<ISickContext>({} as ISickContext);
 export const useSickService = () => useContext(sickServiceContext);
 
 export const SickServiceProvider = ({
@@ -11,6 +13,35 @@ export const SickServiceProvider = ({
   children: ReactNode;
   sickService: SickServiceInterface;
 }) => {
+  const [recentQueryFromStorage, setRecentQueryFromStorage] = useState<string[]>([]); // [1
   const getSickListByQuery = sickService.getSickListByQuery.bind(sickService);
-  return <sickServiceContext.Provider value={{ getSickListByQuery }}>{children}</sickServiceContext.Provider>;
+  useEffect(() => {
+    setRecentQueryFromStorage(JSON.parse(sessionStorage.getItem(sessionStorageQueryListName) || '[]'));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sessionStorage.getItem(sessionStorageQueryListName)]);
+  const setRecentQuery = (query: string) => {
+    const queryList = JSON.parse(sessionStorage.getItem(sessionStorageQueryListName) || '[]');
+    if (queryList.indexOf(query) === -1) {
+      queryList.push(query);
+      sessionStorage.setItem(sessionStorageQueryListName, JSON.stringify(queryList));
+      setRecentQueryFromStorage(queryList);
+    }
+  };
+  useEffect(() => {
+    setRecentQueryFromStorage(JSON.parse(sessionStorage.getItem(sessionStorageQueryListName) || '[]'));
+  }, []);
+
+  return (
+    <sickServiceContext.Provider
+      value={{ getSickListByQuery, recentQuery: recentQueryFromStorage, setRecentQuery: setRecentQuery }}
+    >
+      {children}
+    </sickServiceContext.Provider>
+  );
 };
+
+interface ISickContext {
+  getSickListByQuery: (query: string) => Promise<ISickList>;
+  recentQuery: string[];
+  setRecentQuery: (query: string) => void;
+}
