ad2524ecedaae7e33e356a5d982d8432f858f46c
brince0304 <brince0304@gmail.com>
2023-07-19 14:50:31 +0900

    fix: useCallback 사용하도록 변경

--- src/hooks/useChildBox.tsx
+++ src/hooks/useChildBox.tsx
@@ -1,20 +1,22 @@
-import { RefObject, useEffect, useState } from 'react';
+import { RefObject, useCallback, useEffect, useState } from 'react';
 
 const useChildBox = (ref: RefObject<HTMLElement>) => {
   const [isFocus, setIsFocus] = useState<boolean>(false);
-  const handleClickOutside = (e: MouseEvent) => {
-    if (ref.current && !ref.current.contains(e.target as Node)) {
-      setIsFocus(false);
-    }
-  };
+  const handleClickOutside = useCallback(
+    (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        setIsFocus(false);
+      }
+    },
+    [ref],
+  );
 
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [handleClickOutside]);
 
   return { isFocus, setIsFocus };
 };
